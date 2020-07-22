cd79a90fceda1663b02ce0d0d59b9e8aeebcbc5c
Sujin <sujin.v2px@gmail.com>
2020-07-22 22:43:40 +0545

    task creation

--- src/main/java/com/rxv2px/springbatch/CustomerReportJobConfig.java
+++ src/main/java/com/rxv2px/springbatch/CustomerReportJobConfig.java
@@ -1,5 +1,16 @@
 package com.rxv2px.springbatch;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class CustomerReportJobConfig {
 
     public static final String TASKLET_STEP = "taskletStep";
@@ -7,4 +18,30 @@ public class CustomerReportJobConfig {
     public static final String XML_FILE = "database.xml";
 
     private static final String JOB_NAME = "customerReportJob";
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job customerReportJob() {
+        return jobBuilderFactory.get("customerReportJob")
+                .start(taskletStep())
+                // .next(chunkStep())
+                .build();
+    }
+
+    @Bean
+    public Step taskletStep() {
+        return stepBuilderFactory.get("taskletStep")
+                .tasklet(tasklet())
+                .build();
+    }
+
+
+    public Tasklet tasklet() {
+        return (stepContribution, chunkContext) -> RepeatStatus.FINISHED;
+    }
 }
